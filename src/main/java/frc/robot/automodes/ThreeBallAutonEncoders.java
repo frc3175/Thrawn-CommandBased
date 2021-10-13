8c0074da48daaeff060219917d46c3569a505908
Ian Sjögren <59720505+KnightVision3175@users.noreply.github.com>
2021-10-13 16:30:44 -0400

    auto is fixed

--- src/main/java/frc/robot/automodes/ThreeBallAutonEncoders.java
+++ src/main/java/frc/robot/automodes/ThreeBallAutonEncoders.java
@@ -2,21 +2,22 @@ package frc.robot.automodes;
 
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+//import edu.wpi.first.wpilibj2.command.InstantCommand;
 //import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
+//import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+//import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Constants;
-import frc.robot.autocommands.AutoShootAndHopper;
-import frc.robot.autocommands.AutoShootDuration;
-import frc.robot.autocommands.DriveDistance;
-import frc.robot.commands.IntakeDown;
+//import frc.robot.autocommands.AutoShootAndHopper;
+//import frc.robot.autocommands.AutoShootDuration;
+//import frc.robot.autocommands.DriveDistance;
+//import frc.robot.commands.IntakeDown;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Hopper;
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Shooter;
 
-public class ThreeBallAutonEncoders extends SequentialCommandGroup {
+public class ThreeBallAutonEncoders extends CommandBase {
 
     private Drivetrain m_drivetrain;
     private Shooter m_shooter;
@@ -31,18 +32,20 @@ public class ThreeBallAutonEncoders extends SequentialCommandGroup {
         m_intake = intake;
         addRequirements(m_drivetrain, m_shooter, m_hopper);
 
-        addCommands(
+        /*addCommands(
             new IntakeDown(m_intake),
             new WaitCommand(3.0),
             new DriveDistance(m_drivetrain, Constants.THREE_BALL_DRIVE_SPEED, Constants.THREE_BALL_DRIVE_DISTANCE),
+            new WaitCommand(1.0),
             new AutoShootDuration(m_shooter, Constants.THREE_BALL_SHOOT_SPEED, Constants.THREE_BALL_SPIN_UP),
             new AutoShootAndHopper(m_hopper, m_shooter, Constants.THREE_BALL_HOPPER_SPEED, Constants.THREE_BALL_SHOOT_SPEED, Constants.THREE_BALL_SPIN_UP, Constants.THREE_BALL_HOPPER_RUN),
+            new AutoShootAndHopper(m_hopper, m_shooter, 0, 0, 0.1, 0.1),
             new InstantCommand(m_shooter::StopShooter, m_shooter)
-        );
+        ); */
 
     }
 
-/*
+
     @Override 
     public void initialize() {
         m_timer.reset();
@@ -55,7 +58,7 @@ public class ThreeBallAutonEncoders extends SequentialCommandGroup {
         if(m_timer.get() < 3) {
             m_intake.IntakeDown();
         } else {
-            if(m_drivetrain.getEncoder() > Constants.THREE_BALL_DRIVE_DISTANCE) {
+            if(m_drivetrain.getEncoder() > -Constants.THREE_BALL_DRIVE_DISTANCE) {
                 SmartDashboard.putNumber("Encoders: ", m_drivetrain.getEncoder());
                 m_drivetrain.Drive(Constants.THREE_BALL_DRIVE_SPEED, 0, false);
                 m_shooter.StopShooter();
@@ -86,6 +89,6 @@ public class ThreeBallAutonEncoders extends SequentialCommandGroup {
     public boolean isFinished() {
         return false;
     } 
-*/
+
 
 }
